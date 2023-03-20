809b97bb7ec414c85de5b2ddd5429db29768c97c
VHS35 <110948699+VHS35@users.noreply.github.com>
2023-03-19 23:28:59 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//: Create an Apollo Provider to make every request work with the Apollo server.
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
+    <ApolloProvider client={client}>
+      <Router>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
-    </Router>
+      </Router>
+      </ApolloProvider>
   );
 }
 
