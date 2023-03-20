f1c37ec7a459c017ae7e651844968d91d6c058a8
VHS35 <110948699+VHS35@users.noreply.github.com>
2023-03-19 22:30:04 -0500

    updated server/added schemas/updated auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+//Update the auth middleware function to work with the GraphQL API.
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,7 +7,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,7 +17,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,12 +26,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+      return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
